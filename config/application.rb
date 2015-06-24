cb1f5166d979a35b3d7000ffe0d77abe8b03c755
grador <gradator@gmail.com>
2015-06-24 19:25:23 +0700

    2 версия деплой

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ module Antipmsru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.assets.paths << Rails.root.join('public', 'images')
-    # config.assets.paths << Rails.root.join('app', 'assets','ui-bootstrap','fonts')
-    config.assets.precompile = config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -32,7 +29,19 @@ module Antipmsru
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ru
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+                                          File.extname(path).in? [
+                                                                     '.html', '.erb', '.haml',                 # Templates
+                                                                     # '.png',  '.gif', '.jpg', '.jpeg',         # Images
+                                                                     '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+                                                                 ]
+                                        end)
+    end    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
