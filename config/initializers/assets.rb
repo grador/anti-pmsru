b414ae3bf75f6513acabcde83172b38083dcf406
grador <gradator@gmail.com>
2015-06-24 21:24:45 +0700

    second commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+# Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('public','assets','images')
+# Rails.application.config.assets.paths << Rails.root.join('public','assets','images')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets','ui-bootstrap')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','bower_components')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','bower_components','bootstrap')
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets','bow
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile += %w(*.js *.css *.scss *.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
